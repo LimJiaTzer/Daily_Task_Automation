ff87f68c70ca2560d9803abf1230e3f909e86974
LimJiaTzer <limjiatzer@gmail.com>
2025-04-29 17:31:14 +0800

    Add files via upload

---
+++ Daily_Tasks copy/config.py
@@ -0,0 +1,41 @@
+import datetime
+import os
+
+"""
+----------------------------------
+Edit File locations here
+----------------------------------
+"""
+my_excel_filepath = r""
+
+"""
+----------------------------------
+Edit functions and daily task here
+-> first drop the python file to the same folder as this file
+-> import the function in the same way as the others: from filename import function_name
+-> add the function name to the dictionary/list of tasks in the same way as the others using a tuple: ("task_name", function_name)
+-> for end of day tasks, set the time to after which you want them to run, there is only one time stamp for now
+-> do the exact opposite to remove a task
+----------------------------------
+"""
+
+from sample import taskname
+
+# for daily tasks
+tasks = {
+    "Monday": [("task1", taskname), 
+               ("task2", )],
+    "Tuesday": [], 
+    "Wednesday": [],
+    "Thursday": [],
+    "Friday": [],
+    "Saturday": [],
+    "Sunday": []
+}
+
+# for task after a certain time
+# set your time here
+hour = 17
+minute = 15
+# add your tasks here, it will be ran from left to right
+lasttasksofday = [("longdayatwork", )]
---
+++ Daily_Tasks copy/update_PBI.py
@@ -0,0 +1,71 @@
+import os
+import re
+import time
+import datetime
+import config
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import helper
+
+
+def downloading_supply_analysis() -> bool:
+    downloaded = False
+    while not downloaded:
+        try:
+            today = datetime.datetime.today()
+
+            # Calculate the most recent Monday
+            monday = today - datetime.timedelta(days=today.weekday())
+
+            # Format the date as YYYY-MM-DD
+            monday_date = monday.strftime("%Y-%m-%d")
+            # Set up WebDriver
+            edge_driver_path = r"C:\Users\85210748\Downloads\edgedriver_win32\msedgedriver.exe"
+            service = Service(edge_driver_path)
+            driver = webdriver.Edge(service=service)
+
+            # Open the webpage
+            driver.get("https://eu1.kinaxis.net/web/batp02_prd/RapidResponse/web/default.aspx?logoutRedirect=gwssi&logoutRedirect=gwssi&logoutRedirect=gwssi&logoutRedirect=gwssi&logoutRedirect=gwssi&logoutRedirect=gwssi&ap=1#type=Workbook&name=BAT%20SP%20Planning%20Sheet&scope=public")
+            wait = WebDriverWait(driver, 30) #timeout after a minute
+
+            if not helper.find_and_click_web_element(driver, "//button[@data-test-id='sign-in-react__already-signed-in--continue-button']"):
+                print("No login needed")
+            
+            helper.find_and_click_web_element(driver, "//input[@data-test-id='data-settings-react__filter-item-control-VariableShowAllTabs--picker-control--input--input-control' and @aria-label='Show All Tabs']")
+            helper.find_and_click_web_element(driver, "//div[@data-test-id='data-settings-react__filter-item-control-VariableShowAllTabs--list-item' and @aria-selected='false']")
+            helper.find_and_click_web_element(driver, "//button[@data-test-id='data-settings-react__open']")
+            helper.find_and_click_web_element(driver, "//button[@data-test-id='workbook-react__tab-bar--overflow-menu-icon']")
+            helper.find_and_click_web_element(driver, "//div[@data-test-id='workbook-react__tab-bar--basic-tab-bar--overflow-menu--list-item' and @aria-label='6']")
+            helper.wait_for_element_to_disappear(driver, "//button[@data-test-id='worksheet-react__async-cancel-button']")
+            helper.find_and_click_web_element(driver, "//button[@data-test-id='workbook-react__export-data-button' and @aria-label='ExportData']")
+            helper.find_and_type_into_web_element(driver, "//input[@data-test-id='export-data-workbook-dialog__input-container--input-control']", f"Supply Analysis_{monday_date}", clear_first=True)
+            helper.find_and_click_web_element(driver, "//button[@data-test-id='export-data-workbook-dialog__export-button']")
+            
+            helper.find_and_move_file(config.downloads_folder, config.supply_analysis_folder, "Supply Analysis")
+            downloaded = True
+        except Exception as e:
+            print(f"⚠️ Error occurred: {e}")
+            if helper.proceed("\nDo you want to retry downloading supply analysis file? (y/n): "):
+                driver.quit()
+                continue
+            else:
+                print("Exiting the task...")
+                return False
+    driver.quit()
+    return True
+    
+
+if __name__ == "__main__":
+    print(f"🌟🌟Starting task🌟🌟")
+    if downloading_supply_analysis():
+        print("✅✅ Downloading supply analysis file completed✅✅")
+        input("Hit 'Enter' to exit...")
+    else:
+        print("❌❌ Downloading supply analysis file failed ❌❌")
+        input("Hit 'Enter' to exit...")
